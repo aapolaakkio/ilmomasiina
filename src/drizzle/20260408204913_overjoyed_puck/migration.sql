ce0062cf36a605d66e7a9f9e0d808d9e7fe6bc3c
Aapo Laakkio <aapolaakkio@outlook.com>
2026-04-08 23:50:15 +0300

    fix: add missing migration script

---
+++ src/drizzle/20260408204913_overjoyed_puck/migration.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "enum_audit_event" ADD VALUE 'payment.start';--> statement-breakpoint
+ALTER TYPE "enum_audit_event" ADD VALUE 'payment.complete';--> statement-breakpoint
+ALTER TYPE "enum_audit_event" ADD VALUE 'payment.expire';
